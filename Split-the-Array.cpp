f890931ef4f77d3f4136e1a4aff3a797514281c1
5ashikagupta <98605113+5ashikagupta@users.noreply.github.com>
2024-04-09 23:59:32 +0530

    Update Split-the-Array.cpp

--- Split-the-Array.cpp
+++ Split-the-Array.cpp
@@ -1,3 +1,8 @@
+//You are given an integer array nums of even length. You have to split the array into two parts nums1 and nums2 such that:
+//nums1.length == nums2.length == nums.length / 2.
+//nums1 should contain distinct elements.
+//nums2 should also contain distinct elements.
+//Return true if it is possible to split the array, and false otherwise.
 class Solution {
 public:
     bool isPossibleToSplit(vector<int>& nums) {
